1a11330aa1efed70287dc29b867a19c222dd747b
mac <ahuekweprinceugo@gmail.com>
2026-03-13 19:55:48 +0100

    fix:deploy fix 6

--- src/lib/layout.tsx
+++ src/lib/layout.tsx
@@ -1,15 +1,18 @@
 /** @format */
 
 import Sidebar from '@/components/dashboard/Sidebar';
+import { useState } from 'react';
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className='min-h-screen bg-obsidian-950 flex'>
-      <Sidebar />
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <main className='flex-1 md:ml-64 p-8'>{children}</main>
     </div>
   );
